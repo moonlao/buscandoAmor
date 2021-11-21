0ace1776ceb3d093fb1dbc80344eae03d118e8c6
moonlao <laura.sofia.cobo@gmail.com>
2021-11-21 12:41:06 -0500

    interface finish?

---
+++ app/src/main/java/com/moonlao/buscandoamor/AdoptionRequestActivity.java
@@ -0,0 +1,14 @@
+package com.moonlao.buscandoamor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdoptionRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adoption_request);
+    }
+}
